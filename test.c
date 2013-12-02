76e9dc9d84d8e521e33a8ed50c5172ca6937a665
Jonathan <jonathan.clapson@gmail.com>
2013-12-02 11:13:14 +0000

    merged blocking sleep into test.c (uart bitbang), doesn't seem to be working correctly

--- test.c
+++ test.c
@@ -14,16 +14,59 @@ unsigned int __at(_CONFIG1) configWord1 =
 
 unsigned int __at(_CONFIG2) configWord2 = 0x3fff & (~(1<<8));
 
-#ifndef _XTAL_FREQ
-// This definition is required to calibrate __delay_us() and __delay_ms()
-#define _XTAL_FREQ 4000000
-#endif
-
-#define Baudrate              9600                      //bps
-#define OneBitDelay           (1000000/Baudrate)
+#define Baudrate              1                      //bps
+//#define OneBitDelay           (1000000/Baudrate)
+#define OneBitDelay           (3333)
 #define DataBitCount          8         // no parity, no flow control
 #define UART_RX               RC5	// UART RX pin
 #define UART_TX               RC4	// UART TX pin
+
+typedef union timer_time {
+	char high_byte, low_byte;
+	unsigned int word;
+};
+
+void clock_init(void)
+{
+        //clock is 16MHz
+        SCS1 = 0;
+        SCS0 = 0;
+        IRCF3 = 1;
+        IRCF2 = 1;
+        IRCF1 = 1;
+        IRCF0 = 1;
+}
+
+void timer1_init_1us(void)
+{
+	//set timer 1 to use Fosc/4
+	TMR1CS1 = 0;
+	TMR1CS0 = 0;
+
+	//further divide by 4
+	T1CKPS1 = 1;
+	T1CKPS0 = 0;
+
+	//clock is now 500KHz
+
+	//enable timer
+	TMR1ON = 1;
+}
+
+void usleep(unsigned int num)
+{
+	union timer_time current_time;
+
+	/* reset timer */
+	TMR1L = 0x00;
+	TMR1H = 0x00;
+
+	/* loop until timer expires */
+	do {
+		current_time.low_byte = TMR1L;
+		current_time.high_byte = TMR1H;
+	} while (current_time.word < num);
+}
 
 //Function Declarations
 void InitSoftUART(void);
@@ -32,14 +75,16 @@ void UART_Transmit(const char);
 
 void InitSoftUART(void)		// Initialize UART pins to proper values
 {
-	UART_TX = 1;			// TX pin is high in idle state
-	TRISC = (1<<4);
-}
-
-void __delay_us(unsigned int dly)
-{
-	int i;
-	for (i=0; i<dly/3; i++);
+	/* UART TX Pin is high when idle */
+	UART_TX = 1;			
+//	int b;
+//	b = PORTC;
+//	b ^= (1<<4);
+//	PORTC = b;
+	/* configure uart tx pin as output */
+	TRISC4 = 0;
+	/*configure uart rx pin as input */
+	TRISC5 = 1;
 }
 
 unsigned char UART_Receive(void)
@@ -53,62 +98,75 @@ unsigned char UART_Receive(void)
 	//wait for start bit
 	while(UART_RX==1);
 
-	__delay_us(OneBitDelay);
-	__delay_us(OneBitDelay/2);   // Take sample value in the mid of bit duration
+	usleep(OneBitDelay);
+	usleep(OneBitDelay/2);   // Take sample value in the mid of bit duration
 
 	for ( i = 0; i < DataBitCount; i++ ) {
 		if ( UART_RX == 1 ) { //if received bit is high
 			DataValue += (1<<i);
 		}
-		__delay_us(OneBitDelay);
+		usleep(OneBitDelay);
 	}
 
 	// Check for stop bit
 	if ( UART_RX == 1 ) {      //Stop bit should be high
-		__delay_us(OneBitDelay/2);
+		usleep(OneBitDelay/2);
 		return DataValue;
 	} else {   //some error occurred !
-		__delay_us(OneBitDelay/2);
+		usleep(OneBitDelay/2);
 		return 0x000;
 	}
 }
 
 void UART_Transmit(const char DataValue)
 {
-	/* Basic Logic TX pin is usually high. A high to low bit is the starting bit 
-and a low to high bit is the ending bit. No parity bit. No flow Control.
+	/* Basic Logic TX pin is usually high. 
+	A high to low bit is the starting bit and a low to high bit is 
+	the ending bit. No parity bit. No flow Control.
+	BitCount is the number of bits to transmit. Data is 
+	transmitted LSB first. 	*/
 
-	   BitCount is the number of bits to transmit. Data is transmitted LSB first.
-	*/
-	unsigned char i;
+/*	unsigned char i;
 
 	// Send Start Bit
 	UART_TX = 0;
-	__delay_us(OneBitDelay);
+	usleep(OneBitDelay);
 
 	for ( i = 0; i < DataBitCount; i++ ) {
 		//Set Data pin according to the DataValue
-		if( ((DataValue>>i)&0x1) == 0x1 ) {  //if Bit is high
+		if((DataValue>>i) &0x1) {  //if Bit is high
 			UART_TX = 1;
 		} else {     //if Bit is low
 			UART_TX = 0;
 		}
-	    	__delay_us(OneBitDelay);
+	    	usleep(OneBitDelay);
 	}
 
 	//Send Stop Bit
 	UART_TX = 1;
-	__delay_us(OneBitDelay);
+	usleep(OneBitDelay);*/
 }
 
 void main()
 {
-
 	unsigned char ch = 0;
+	unsigned char rc_val = 1;
+	unsigned char i;
 
 	InitSoftUART();
 
+	TRISC2 = 0;
+	RC2 = rc_val;
+
+	clock_init();
+	timer1_init_1us();
+
 	while(1){
-		UART_Transmit(0xAA);
+//		UART_Transmit(0x65);
+		for (i=0; i<100; i++)
+			usleep(10000);
+	
+		rc_val = !rc_val;
+		RC2 = rc_val;
 	}
 }
