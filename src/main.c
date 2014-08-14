9afdb98e98c47b0f8c9b63584a9f354fd854551b
Jonathan Clapson <jonathan.clapson@gmail.com>
2014-08-14 23:16:20 +1200

    main.c missing from last commit. main's file header is updated as it has different functionaility to example now. uart now prints 'a' repetatively in main loop

--- src/main.c
+++ src/main.c
@@ -1,22 +1,15 @@
-/*
- * USB HID Mouse
+/**
+ * @file main.c
+ * @brief Transmit received IR via USB
  *
- * This file may be used by anyone for any purpose and may be used as a
- * starting point making your own application using M-Stack.
+ * Program which receives NEC IR commands and transmits
+ * them to host device via USB HID interface
  *
- * It is worth noting that M-Stack itself is not under the same license as
- * this file.
+ * Adapted from signal11's m-stack main.c
  *
- * M-Stack is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  For details, see sections 7, 8, and 9
- * of the Apache License, version 2.0 which apply to this file.  If you have
- * purchased a commercial license for this software from Signal 11 Software,
- * your commerical license superceeds the information in this header.
- *
- * Alan Ott
- * Signal 11 Software
- * 2013-08-13
+ * @author Jonathan Clapson jonathan.clapson@gmail.com
+ * @date 2014
+ * @copyright GNU GENERAL PUBLIC LICENSE version 2
  */
 
 #include "usb.h"
@@ -26,36 +19,11 @@
 #include "usb_ch9.h"
 #include "usb_hid.h"
 
-#ifdef __PIC32MX__
-	#include <plib.h>
-#endif
+/* use hardware uart implementation */
+#define PIC_USE_HARD_UART
+#include "uart.h"
 
-#ifdef __PIC24FJ64GB002__
-_CONFIG1(WDTPS_PS16 & FWPSA_PR32 & WINDIS_OFF & FWDTEN_OFF & ICS_PGx1 & GWRP_OFF & GCP_OFF & JTAGEN_OFF)
-_CONFIG2(POSCMOD_NONE & I2C1SEL_PRI & IOL1WAY_OFF & OSCIOFNC_OFF & FCKSM_CSDCMD & FNOSC_FRCPLL & PLL96MHZ_ON & PLLDIV_NODIV & IESO_OFF)
-_CONFIG3(WPFP_WPFP0 & SOSCSEL_IO & WUTSEL_LEG & WPDIS_WPDIS & WPCFG_WPCFGDIS & WPEND_WPENDMEM)
-_CONFIG4(DSWDTPS_DSWDTPS3 & DSWDTOSC_SOSC & RTCOSC_SOSC & DSBOREN_OFF & DSWDTEN_OFF)
-
-#elif __PIC24FJ256DA206__
-_CONFIG1(WDTPS_PS32768 & FWPSA_PR128 & WINDIS_OFF & FWDTEN_OFF & ICS_PGx2 & GWRP_OFF & GCP_OFF & JTAGEN_OFF)
-_CONFIG2(POSCMOD_NONE & IOL1WAY_OFF & OSCIOFNC_ON & FCKSM_CSECMD & FNOSC_FRCPLL & PLL96MHZ_ON & PLLDIV_NODIV & IESO_OFF)
-_CONFIG3(WPFP_WPFP255 & SOSCSEL_SOSC & WUTSEL_LEG & ALTPMP_ALPMPDIS & WPDIS_WPDIS & WPCFG_WPCFGDIS & WPEND_WPENDMEM)
-
-#elif _18F46J50
-#pragma config PLLDIV = 3 /* 3 = Divide by 3. 12MHz crystal => 4MHz */
-#pragma config XINST = OFF
-#pragma config WDTEN = OFF
-#pragma config CPUDIV = OSC1
-#pragma config IESO = OFF
-#pragma config FCMEN = OFF
-#pragma config LPT1OSC = OFF
-#pragma config T1DIG = ON
-#pragma config OSC = ECPLL
-#pragma config DSWDTEN = OFF
-#pragma config IOL1WAY = OFF
-#pragma config WPDIS = OFF /* This pragma seems backwards */
-
-#elif _16F1454 || _16F1455 || _16F1459
+#if defined(_16F1454) || defined(_16F1455) || defined(_16F1459)
 #pragma config FOSC = INTOSC
 #pragma config WDTE = OFF
 #pragma config PWRTE = OFF
@@ -74,18 +42,6 @@ _CONFIG3(WPFP_WPFP255 & SOSCSEL_SOSC & WUTSEL_LEG & ALTPMP_ALPMPDIS & WPDIS_WPDI
 #pragma config BORV = LO
 #pragma config LPBOR = ON
 #pragma config LVP = ON
-
-#elif __32MX460F512L__
-#pragma config DEBUG = OFF, ICESEL = ICS_PGx2, PWP = OFF, BWP = OFF, CP = OFF
-#pragma config FNOSC = PRIPLL, FSOSCEN = OFF, IESO = OFF, POSCMOD = HS, \
-	       OSCIOFNC = OFF, FPBDIV = DIV_1, FCKSM = CSDCMD, WDTPS = PS1, \
-	       FWDTEN = OFF
-#pragma config FPLLIDIV = DIV_2, FPLLMUL = MUL_15, UPLLIDIV = DIV_2, \
-	       UPLLEN = ON, FPLLODIV = DIV_1
-
-#else
-	#error "Config flags for your device not defined"
-
 #endif
 
 #ifdef MULTI_CLASS_DEVICE
@@ -94,23 +50,12 @@ static uint8_t hid_interfaces[] = { 0 };
 
 int main(void)
 {
-#if defined(__PIC24FJ64GB002__) || defined(__PIC24FJ256DA206__)
-	unsigned int pll_startup_counter = 600;
-	CLKDIVbits.PLLEN = 1;
-	while(pll_startup_counter--);
-#elif _18F46J50
-	unsigned int pll_startup = 600;
-	OSCTUNEbits.PLLEN = 1;
-	while (pll_startup--)
-		;
-#elif _16F1454 || _16F1455 || _16F1459 
+#if defined(_16F1454) || defined(_16F1455) || defined(_16F1459)
 	OSCCONbits.IRCF = 0b1111; /* 0b1111 = 16MHz HFINTOSC postscalar */
 
 	/* Enable Active clock-tuning from the USB */
 	ACTCONbits.ACTSRC = 1; /* 1=USB */
 	ACTCONbits.ACTEN = 1;
-#elif __32MX460F512L__
-	SYSTEMConfigPerformance(80000000);
 #endif
 
 
@@ -119,17 +64,15 @@ int main(void)
 	#if defined (_PIC18) || defined(_PIC14E)
 		INTCONbits.PEIE = 1;
 		INTCONbits.GIE = 1;
-	#elif __PIC32MX__
-		INTCONbits.MVEC = 1; /* Multi-vector interrupts */
-		IPC11bits.USBIP = 4; /* Interrupt priority, must set to != 0. */
-		__asm volatile("ei");
-	#endif
+	#endif	
 #endif
 
 #ifdef MULTI_CLASS_DEVICE
 	hid_set_interface_list(hid_interfaces, sizeof(hid_interfaces));
 #endif
 	usb_init();
+	
+	uart_init();
 
 	/* Setup mouse movement. This implementation sends back data for every
 	 * IN packet, but sends no movement for all but every delay-th frame.
@@ -147,12 +90,22 @@ int main(void)
 	uint8_t x_count = 100;
 	uint8_t delay = 7;
 	int8_t x_direc = 1;
-
-	while (1) {
+	
+	char rc_val = 0;
+	// init LED
+	TRISC2 = 0;
+	RC2 = rc_val;
+	
+	while (1) {		
+		
+		/* NOTE: Code in here will not run until USB is plugged in! */
 		if (usb_is_configured() &&
 		    !usb_in_endpoint_halted(1) &&
 		    !usb_in_endpoint_busy(1)) {
-
+		    	uart_putc('a');
+		    	rc_val = rc_val?0:1;
+			RC2 = rc_val;
+	
 //			unsigned char *buf = usb_get_in_buffer(1);
 			struct remote_buf_t* remote_buf = usb_get_in_buffer(1);
 			unsigned char *buf = (unsigned char *) remote_buf;
